4ebc8db67a219d19c60f9229ecc7b636d14aa4f4
summekoushal <2020bcs003@sggs.ac.in>
2023-06-12 10:55:29 +0530

    added maps

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
-import {
-  GoogleMap,
-  Marker,
-  useLoadScript,
-  useJsApiLoader,
-} from "@react-google-maps/api";
 
 function App() {
   return (
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,26 +1,32 @@
 import React from "react";
 import PickDrop from "./PickDrop";
-import { GoogleMap, Marker, useLoadScript, useJsApiLoader } from "@react-google-maps/api";
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+
+const center = { lat: 48.8584, lng: 2.2945 };
 
 export default function MainContent() {
-  function initMap() {
-    const tutorialsPoint_office = {
-      lat: 17.43827944602866,
-      lng: 78.39530424154626,
-    };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 17.56,
-      center: tutorialsPoint_office,
-    });
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyBwDZTFxoP3Y_nbS_D - UZUZoAAtDSSKv6s",
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div className="MainContainer col">
-      <div className="findTrip col-3">
+      {/* <div className="contain"> */}
+      <div className="findTrip col-2">
         <h5 className="PickDropItem">Find a Trip</h5>
         <PickDrop />
       </div>
-      <div className="col-10" id="map"></div>
+      <div className="map col-7">
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "77vh" }}
+        ></GoogleMap>
+      </div>
     </div>
-  );
+  )
 }
